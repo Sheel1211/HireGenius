2d9a55a3183f6ef3a2012d350ec389fb381a81ac
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2023-08-24 11:31:28 +0530

    Revert "added compiler and landing page"
    This reverts commit 594cd9af9cbeef3e26278647ee6575baaa37fa90.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,37 +2,14 @@ import React from "react";
 import Signin from "./pages/auth/Signin";
 import "./index.css";
 import FormBuilder from "./pages/FormBuilder/Main";
-import CsvParser from "./pages/CsvParser/Main";
 import StudentAptitude from "./pages/Student/Aptitude";
-import Playground from './pages/compiler/Playground';
-import Landing from "./pages/landing_page/Landing";
-import CompilerHome from './pages/compiler/Home'
-import { Route,BrowserRouter as Router, Routes } from "react-router-dom";
-import Header from "./pages/landing_page/header/Header";
+
 const App = () => {
   return (
-    // <>
-
-
-    //   {/* <FormBuilder /> */}
-    //   <Landing />
-    //   {/* <CsvParser /> */}
-    //   {/* <StudentAptitude /> */}
-    // </>
-
-    <Router>
-       <Header />
-      <Routes>
-        
-        <Route path="/" exact element={<Landing />} />
-        <Route path="/compiler" element={<CompilerHome />} />
-        <Route path="/compiler/playground" element={<Playground />} />
-        <Route path="/form" element={<FormBuilder /> } />
-        <Route path="/parser" element={<CsvParser /> } />
-        <Route path="/apt" element={<StudentAptitude /> } />
-        
-      </Routes>
-    </Router>
+    <>
+      <FormBuilder />
+      {/* <StudentAptitude /> */}
+    </>
   );
 };
 
