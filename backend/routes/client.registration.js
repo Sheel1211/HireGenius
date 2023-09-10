fa40347d5f14d49fbcf70ed79484489eb8d95b27
Barot Pratham Janakkumar <barotpratham30@gmail.com>
2023-09-10 20:12:43 +0530

    add service folder

--- backend/routes/client.registration.js
+++ backend/routes/client.registration.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const fileUpload = require('express-fileupload');
-const AWS = require('aws-sdk')
-
-router.use(fileUpload());
-
-//API -> /api/client/upload
-router.post('/upload',async(req,res)=>{
-   
-})
-
+const {uploadFile}  = require('../services/uploadFileS3.service')
 
 //1. Registration 
 // API -> /api/client/registration
-
 router.post("/registration",async(req,res)=>{
 
     const clientType = req.body.clienttype;
@@ -23,51 +13,29 @@ router.post("/registration",async(req,res)=>{
     
     console.log("data",clientType,company,domain,website)
 
-    AWS.config.update({
-        accessKeyId:process.env.ACCESS_KEY_ID,
-        secretAccessKey:process.env.SECRET_ACCESS_KEY,
-        region:process.env.REGION
-    })
+    const imgUrl = await uploadFile(req.files)
 
-    const s3 = new AWS.S3();
+    console.log("Data : ",clientType,company,domain,website,imgUrl.Location)
 
-    const fileContent = Buffer.from(req.files.data.data,'binary');
-
-    const params = {
-        Bucket: 'hireginus-images',
-        Key:`${new Date().getTime().toString()}`,
-        Body: fileContent
-    }
-
-
-    s3.upload(params,(err,data)=>{
-        if(err){
-            throw err;
-        }
-        res.send({
-            "response_code":200,
-            "response_message":"Success",
-            "response_data":data,
-        });
-    })
+    res.status(200).send({message:"Success"})
 
 })
 
 
 //2. Post plan
 // API -> /api/client/post-plan
-
 router.post("/post-plan",(req,res)=>{
     const data = req.body
     console.log("DATA : ",data)
     res.status(200).send({message:"Successfully"})
 }) 
 
+
 //3. Pree plan 
 // API -> /api/client/pree-plan
-
 router.post("/pree-plan",(req,res)=>{
     res.send("pree registration of client");
 })
+
 
 module.exports = router;
