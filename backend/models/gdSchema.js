6fa1fff331b0afc8d1229e914173b55b9a72d13b
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2023-09-12 14:56:38 +0530

    reverted changes

--- backend/models/gdSchema.js
+++ backend/models/gdSchema.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose')
 
 const gdSchema = new mongoose.Schema({
   interviewId: {
--- backend/models/interviewSchema.js
+++ backend/models/interviewSchema.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose')
 
 const interviewSchema = new mongoose.Schema({
   type: {
