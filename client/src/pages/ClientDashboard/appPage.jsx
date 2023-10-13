26eae8bf4a16bb2e58b91c00d71dcb714ab508b2
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2023-10-13 11:00:16 +0530

    generated view candidates button

--- client/src/pages/ClientDashboard/appPage.jsx
+++ client/src/pages/ClientDashboard/appPage.jsx
@@ -1,28 +1,8 @@
-import { Helmet } from "react-helmet-async";
-import { DataGrid } from "@mui/x-data-grid";
-import { faker } from "@faker-js/faker";
-// @mui
-import { useTheme } from "@mui/material/styles";
-import { Grid, Container, Typography, Button } from "@mui/material";
-// components
-import Iconify from "./components/iconify";
-// sections
-import AppTasks from "./sections/app/appTask";
-import { useSelector } from "react-redux";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Navigate, useNavigate } from "react-router-dom";
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
+import { Container, Typography, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const config = {
   headers: {
@@ -34,47 +14,81 @@ const config = {
   withCredentials: true,
 };
 
+// CandidateListTable component for displaying candidate information
 const CandidateListTable = ({ candidates }) => {
-  // console.log(candidates);
   return (
-    <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="candidate table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell>Email</TableCell>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="candidate table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Email</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {candidates.map((candidate, index) => (
+            <TableRow key={index}>
+              <TableCell>{candidate[0].name}</TableCell>
+              <TableCell>{candidate[0].email}</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {/* {console.log("candidates", candidates)} */}
-            {candidates.map((candidate, index) =>
-              candidate.map((c, key) => (
-                <TableRow key={key}>
-                  <TableCell>{c.name}</TableCell>
-                  <TableCell>{c.email}</TableCell>
-                </TableRow>
-              ))
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
-export default function DashboardAppPage() {
-  const theme = useTheme();
-  const user = useSelector((state) => state.User);
+const DashboardAppPage = () => {
   const [allInterviews, setAllInterviews] = useState([]);
-  const [candidates, setCandidates] = useState([]);
+  const [selectedCandidates, setSelectedCandidates] = useState([]);
+  const [showCandidates, setShowCandidates] = useState(false);
   const navigate = useNavigate();
-  const sendEmailWithLink = (candidates) => {
+  const user = useSelector((state) => state.User);
+
+  // Fetch all interviews and set them to the state
+  const fetchInterviews = () => {
+    axios
+      .get("http://127.0.0.1:4000/api/interview/all-interviews", config)
+      .then((res) => {
+        setAllInterviews(res.data.interviews);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  useEffect(() => {
+    fetchInterviews();
+  }, []);
+
+  
+  const allData = (candidates) => {
+    const candidateIds = candidates.map((data) => data.candidateId);
+    
+    
+    const baseUrl = "http://127.0.0.1:4000/api/candidate/getCandidate";
+    const requests = candidateIds.map((candidateId) => {
+      return axios.post(baseUrl, { candidateId }, config);
+    });
+
+    
+    Promise.all(requests)
+      .then((responses) => {
+        const candidateAllDetails = responses.map((response) => response.data.candidate);
+        setSelectedCandidates(candidateAllDetails);
+        setShowCandidates(!showCandidates); 
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const sendEmailWithLink = (candidates,interviewId) => {
+    
     const aptLink = localStorage.getItem("AptitudeLink");
+    console.log(candidates)
     axios
       .post(
         "http://127.0.0.1:4000/api/interview/sendemail-to-candidates",
-        { aptLink, candidates,interviewId },
+        { aptLink, candidates, interviewId },
         config
       )
       .then((res) => {})
@@ -83,105 +97,65 @@ export default function DashboardAppPage() {
       });
   };
 
-  const [showCandidates, setShowCandidates] = useState(false);
-  const [selectedCandidates, setSelectedCandidates] = useState([]);
-  const [allCandidates, setAllCandidates] = useState([]);
-
-  const allData = (candidates) => {
-    const candidateIds = [];
-
-    candidates.map((data) => candidateIds.push(data.candidateId));
-    console.log("Here is array :" + candidateIds);
-    const baseUrl = "http://127.0.0.1:4000/api/candidate/getCandidate";
-    const requests = candidateIds.map((candidateId) => {
-      return axios.post(baseUrl, { candidateId }, config);
-    });
-
-    const candidateAllDetails = [];
-
-    Promise.all(requests)
-      .then((res) => {
-        res.forEach((re, index) => {
-          console.log(re, index);
-          candidateAllDetails.push(re.data.candidate);
-          console.log(
-            "all candidate details after request: ",
-            candidateAllDetails
-          );
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    setAllCandidates(candidateAllDetails);
-  };
-
-  const handleViewCandidates = (candidates) => {
-    setSelectedCandidates(allCandidates);
-    setShowCandidates(!showCandidates);
-  };
-
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:4000/api/interview/all-interviews", config)
-      .then((res) => {
-        console.log(res.data);
-        setAllInterviews(res.data.interviews);
-      })
-      .catch((e) => console.log(e));
-  }, []);
-
   return (
-    <>
-      <Container maxWidth="xl">
-        <Typography variant="h4" sx={{ mb: 5 }}>
-          Hi, Welcome {user.User.email}
-        </Typography>
-        {allInterviews && (
-          <Grid container spacing={3}>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Title</TableCell>
-                    <TableCell align="right">Candidates</TableCell>
-                    <TableCell align="right">Rounds</TableCell>
-                    <TableCell align="right">Options</TableCell>
+    <Container maxWidth="xl">
+      <Typography variant="h4" sx={{ mb: 5 }}>
+        Hi, Welcome {user.User.email}
+      </Typography>
+      {allInterviews && (
+        <Grid container spacing={3}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Title</TableCell>
+                  <TableCell align="right">Candidates</TableCell>
+                  <TableCell align="right">Rounds</TableCell>
+                  <TableCell align="right">Options</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {allInterviews.map((row) => (
+                  <TableRow
+                    key={row._id}
+                    sx={{
+                      "&:last-child td, &:last-child th": { border: 0 },
+                      cursor: "pointer",
+                    }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.title}
+                    </TableCell>
+                    <TableCell align="right">{row.candidates.length}</TableCell>
+                    <TableCell align="right">{row.rounds.length}</TableCell>
+                    <TableCell align="right">
+                      <Button onClick={() => allData(row.candidates)}>
+                        View candidates
+                      </Button>
+                      <Button onClick={() => sendEmailWithLink(row.candidates,row._id)}>
+                        Send Email
+                      </Button>
+                      <Button
+                        onClick={() => navigate("/clientdashboard/schedule-interview")}
+                      >
+                        Schedule Interview
+                      </Button>
+                    </TableCell>
                   </TableRow>
-                </TableHead>
-                <TableBody>
-                  {allInterviews.map((row) => (
-                    <TableRow
-                      key={row._id}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 },cursor: 'pointer', }}
-                      onClick={() => {
-                        sendEmailWithLink(row.candidates);
-                        navigate("/clientdashboard/schedule-interview");
-                      }}
-                    >
-                      <TableCell component="th" scope="row">
-                        {row.title}
-                      </TableCell>
-                      <TableCell align="right">
-                        {row.candidates.length}
-                      </TableCell>
-                      <TableCell align="right">{row.rounds.length}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            {showCandidates && (
-              <div>
-                <h2>Candidates:</h2>
-                {/* {console.log("selected andis", selectedCandidates)} */}
-                <CandidateListTable candidates={selectedCandidates} />
-              </div>
-            )}
-          </Grid>
-        )}
-      </Container>
-    </>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          {showCandidates && selectedCandidates.length > 0 && (
+            <div>
+              <h2>Candidates:</h2>
+              <CandidateListTable candidates={selectedCandidates} />
+            </div>
+          )}
+        </Grid>
+      )}
+    </Container>
   );
-}
+};
+
+export default DashboardAppPage;
